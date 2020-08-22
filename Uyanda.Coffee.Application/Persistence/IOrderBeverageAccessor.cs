cd3ae9880aa00a701b8b9ce2daf7fbf4ffd3917f
uyandam <uyandamphunga@gmail.com>
2020-08-22 23:11:05 +0200

    add persistence and application classes

---
+++ Uyanda.Coffee.Application/Persistence/IOrderBeverageAccessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uyanda.Coffee.Application.Features.AddBeverage;
+
+namespace Uyanda.Coffee.Application.Persistence
+{
+    public interface IOrderBeverageAccessor
+    {
+        void MakeOrder(AddBeverageCommand drinkOrder);
+        string ViewOrder();
+    }
+}
