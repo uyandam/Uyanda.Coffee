d092a651af01c79ee958581e7880462cf46059ca
uyandam <uyandamphunga@gmail.com>
2020-08-23 18:27:29 +0200

    trying to implement xUnit

--- Uyanda.Coffee.Persistence/Class1.cs
+++ Uyanda.Coffee.Persistence/Class1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Uyanda.Coffee.Persistence
 {
-    class LocalDbContext
+    public class Class1
     {
     }
 }
--- Uyanda.Coffee.Persistence/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Uyanda.Coffee.Persistence
-{
-    public class Constants
-    {
-    }
-}
